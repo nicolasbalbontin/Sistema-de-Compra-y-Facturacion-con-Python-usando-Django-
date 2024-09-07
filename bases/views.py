72cdde5906a0794c82582ef5dc07ad7d5f40c2ac
nicolas balbontin <nicolas.balbontin@gmail.com>
2024-09-07 13:56:35 -0400

    Creando Login

--- bases/views.py
+++ bases/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
 # Create your views here.
+
+
+class Home(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'bases/home.html'
+    login_url = '/login'
+    login_redirect_url = '/'
--- src/settings.py
+++ src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bases',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+REDIRECT_LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
